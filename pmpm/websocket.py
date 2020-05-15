5dffcb4950221c5cf311d156b10d545efcc8c1a7
Sebastian Weichwald <git@sweichwald.de>
2020-05-15 15:05:09 +0200

    Add clarifying comment.
    Signed-off-by: Sebastian Weichwald <git@sweichwald.de>

--- pmpm/websocket.py
+++ pmpm/websocket.py
@@ -263,6 +263,8 @@ def item_generator(json_input, lookup_key):
 
 @lru_cache(maxsize=LRU_CACHE_SIZE)
 def bibliography(blocks, cwd, bibfile):
+    # Collect the cite-keys while keeping the order and
+    # dropping duplicates
     cites = dict.fromkeys(
         item_generator(json.loads(blocks), "citationId")).keys()
     bibcontent = ("---\n"
